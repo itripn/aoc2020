e116e449d4928aa995c88169e7b1b89079da6ded
rjf <rjf>
2020-12-05 11:24:29 -0800

    cleanup

--- five/src/main.rs
+++ five/src/main.rs
@@ -1,3 +1,23 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut boardingpasses : Vec<String> = Vec::new();
+
+    if let Ok(lines) = read_lines("./input.txt") {
+        // Consumes the iterator, returns an (Optional) String
+
+        let mut str_buf : String = String::new();
+
+        for line in lines {
+
+            if let Ok(bpline) = line {
+
+            }
+        }
+    }
+
 }
--- four/src/main.rs
+++ four/src/main.rs
@@ -2,6 +2,7 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use regex::Regex;
 
 // ecl:gry pid:860033327 eyr:2020 hcl:#fffffd       // Valid
 // byr:1937 iyr:2017 cid:147 hgt:183cm
@@ -48,19 +49,24 @@ fn main() {
         }
     }
 
+    ////////////////////////////////////////////////////////////////
+    ///// FIXME -- code is counting ONE extra passport as valid! ///
+    ////////////////////////////////////////////////////////////////
     let valid_passports = count_valid_passports( &ppdata ) ;
     println!( "Valid passports: {}", valid_passports ); 
 }
 
-// byr (Birth Year)
-// iyr (Issue Year)
-// eyr (Expiration Year)
-// hgt (Height)
-// hcl (Hair Color)
-// ecl (Eye Color)
-// pid (Passport ID)
-// cid (Country ID)
-// 
+
+// byr (Birth Year) - four digits; at least 1920 and at most 2002.
+// iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+// eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+// hgt (Height) - a number followed by either cm or in:
+// If cm, the number must be at least 150 and at most 193.
+// If in, the number must be at least 59 and at most 76.
+// hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+// ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+// pid (Passport ID) - a nine-digit number, including leading zeroes.
+// cid (Country ID) - ignored, missing or not.
 
 fn count_valid_passports( passports : &Vec<String> ) -> i32 {
 
@@ -68,21 +74,124 @@ fn count_valid_passports( passports : &Vec<String> ) -> i32 {
 
     for passport in passports {
 
-        let byr = passport.contains( "byr:" );
-        let iyr = passport.contains( "iyr:" );
-        let eyr = passport.contains( "eyr:" );
-        let hgt = passport.contains( "hgt:" );
-        let hcl = passport.contains( "hcl:" );
-        let ecl = passport.contains( "ecl:" );
-        let pid = passport.contains( "pid:" );
+        let byr = passport.contains( "byr:" ) && validate_year( &passport, "byr:", (1920, 2002) );
+        let iyr = passport.contains( "iyr:" ) && validate_year( &passport, "iyr:", (2010, 2020) );
+        let eyr = passport.contains( "eyr:" ) && validate_year( &passport, "eyr:", (2020, 2030) );
+        let hgt = passport.contains( "hgt:" ) && validate_height( &passport, (150,193,59,76) );
+        let hcl = passport.contains( "hcl:" ) && validate_hair_color( &passport );
+        let ecl = passport.contains( "ecl:" ) && validate_eye_color( &passport );
+        let pid = passport.contains( "pid:" ) && validate_pid( &passport );
         
+        // println!( "{} {} {} {} {} {} {}", byr, iyr, eyr, hgt, hcl, ecl, pid );
         if byr && iyr && eyr && hgt && hcl && ecl && pid {
             valid_count = valid_count + 1;
+        }
+        else {
+            // println!( "bad: {}\n", passport );
         }
     }
 
     valid_count
 }
+
+fn validate_pid( passport : &String ) -> bool {
+
+    let re = Regex::new(r"pid:(\d{9})").unwrap();
+    let cap = re.captures( passport );
+
+    match cap {
+
+        Some(_c) => {
+            // println!( "pid: {}", c.get(1).unwrap().as_str() );
+            true
+        },
+
+        None => { /*println!("bad pp: {}", passport );*/ false }
+    }
+}
+
+fn validate_hair_color( passport : &String ) -> bool {
+
+    let re = Regex::new( r"hcl:(\#[0-9a-f]{6})" ).unwrap();
+    let cap = re.captures( passport );
+    match cap {
+        Some(_c) => { /*println!( "hcl: {}", c.get(1).unwrap().as_str() );*/ return true; },
+        None => { /*println!("bad hcl: {}", passport);*/ false }
+    }
+}
+
+fn validate_eye_color( passport : &String ) -> bool {
+
+    let re = Regex::new( r"ecl:(amb|blu|brn|gry|grn|hzl|oth)" ).unwrap();
+    let cap = re.captures( passport );
+    match cap {
+        Some(_c) => { /*println!( "ecl: {}", _c.get(1).unwrap().as_str() );*/ return true; },
+        None => { /*println!("bad ecl: {}", passport );*/ false}
+    }
+}
+
+fn validate_height( passport : &String, bounds : (i32,i32,i32,i32) ) -> bool {
+
+    let re = Regex::new( r"hgt:(\d{2,3})(cm|in)" ).unwrap();
+    let cap = re.captures( passport );
+    match cap {
+
+        Some(c) => { 
+
+            let hgt_str = c.get(1).unwrap().as_str();
+            let unit_str = c.get(2).unwrap().as_str();
+
+            let hgt = hgt_str.parse::<i32>().unwrap();
+            let good_height =  match unit_str {
+
+                "cm" =>  hgt >= bounds.0 && hgt <= bounds.1,
+                "in" =>  hgt >= bounds.2 && hgt <= bounds.3,
+                _ => return false
+            };
+
+            // println!("height: {}", good_height );
+            if !good_height {
+                println!("bad hgt: {}\n", passport );
+            }
+
+            good_height
+        },
+        None => false
+    }
+}
+
+fn get_regex( field : &str ) -> &str {
+
+    match field {
+
+        "iyr:" => r"iyr:(\d{4})",
+        "eyr:" => r"eyr:(\d{4})",
+        "byr:" => r"byr:(\d{4})",
+        _ => ""
+    }
+}
+
+fn validate_year( passport : &String, field : &str, bounds : (i32, i32) ) -> bool {
+
+    let re = Regex::new(get_regex( field ) ).unwrap();
+    let cap = re.captures( passport );
+    match cap {
+
+        Some(c) => { 
+
+            let year_str = c.get(1).unwrap().as_str();
+            let year = year_str.parse::<i32>().unwrap();
+
+            let good_year = year >= bounds.0 && year <= bounds.1;
+            // println!("year: {}, ({} - {},{})", good_year, year, bounds.0, bounds.1 );
+
+            good_year
+        },
+
+        None => false
+    }
+}
+
 
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
