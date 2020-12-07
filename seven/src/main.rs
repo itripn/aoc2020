b41c1e586f2d1980186d5d8cbeafd8b268f01331
rjf <rjf>
2020-12-06 21:15:17 -0800

    adding seven in

---
+++ seven/src/main.rs
@@ -0,0 +1,29 @@
+
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+use std::collections::BTreeMap;
+
+fn main() {
+
+   if let Ok(lines) = read_lines("./input.txt") {
+
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+
+            // Collapse all lines for a group into a single line
+            if let Ok(l) = line {
+
+            }
+        }
+    }
+
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
