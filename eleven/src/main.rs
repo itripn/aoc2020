f18c2788fd7d96079b631fd4d040fa1a83523e28
Ron Forrester <rjf@m1.home>
2020-12-12 13:26:40 -0800

    adding 11a solution

--- eleven/src/main.rs
+++ eleven/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
 
     // Read lines into a vector
     //
-    if let Ok(lines) = read_lines("./input.txt") {
+    if let Ok(lines) = read_lines("input.txt") {
 
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
@@ -23,34 +23,143 @@ fn main() {
         }
     }
 
-    let mut seated;
-    let mut last_seated = 0;
+    let mut last_seated_count = dump_seat_map( &data );
+    let mut rounds = 1;
+
     loop {
 
-        seated = perform_round( &mut data );
-        if last_seated == seated {
-            break;
+        data = perform_round( &mut data );
+        println!("\n**** After round: {} **********************************************", rounds );
+        
+        let seated_count = dump_seat_map( &data );
+
+        if seated_count != last_seated_count {
+            last_seated_count = seated_count;
         }
         else {
-            last_seated = seated;
+            println!("Final seated count: {}", seated_count );
+            break;
+        }
+
+        rounds = rounds + 1;
+    }
+}
+
+fn perform_round( cur_seat_map : &mut Vec<Vec<char>> ) -> Vec<Vec<char>> {
+
+    let mut new_seat_map = cur_seat_map.clone();
+
+    for row in 0..cur_seat_map.len() {
+
+        for col in 0..cur_seat_map[row].len() {
+
+            let curseat = cur_seat_map[row][col];
+
+            if curseat == 'L' && is_adjacent_to_seat_type( '#', row, col, &cur_seat_map ) == 0 {
+                new_seat_map[row][col] = '#';
+            }
+            else if curseat == '#' && is_adjacent_to_seat_type( '#', row, col, &cur_seat_map ) >=4 {
+                new_seat_map[row][col] = 'L';
+            }
+            else  {
+                new_seat_map[row][col] = curseat;
+            }
         }
     }
+
+    new_seat_map
 }
 
-fn perform_round( seat_map : &mut Vec<Vec<char>> ) -> i32 {
+fn is_adjacent_to_seat_type( seat_type : char, row : usize, col : usize, map : &Vec<Vec<char>> ) -> i32 {
 
-    let mut row : usize = 0;
-    let mut col : usize = 0;
-    let mut seated_count = 0;
+    let mut adjacent_count = 0;
 
-    loop {
+    // Cover seat to left if there is one
+    //
+    if col > 0 {
+        if map[row][col-1] == seat_type {
+            adjacent_count += 1;
+        }
+    }    
 
-        // execute rules
+    // Cover seat to right if there is one
+    //
+    if col < map[row].len()-1 {
+        if map[row][col+1] == seat_type {
+            adjacent_count += 1;
+        }
     }
 
-    seated_count
+    // Cover seat above if there is one
+    //
+    if row > 0 {
+        if map[row-1][col] == seat_type {
+            adjacent_count += 1;
+        }
+    }
+
+    // Cover seat below if there is one
+    //
+    if row < map.len()-1 {
+        if map[row+1][col] == seat_type {
+            adjacent_count += 1;
+        }
+    }
+
+    // Cover diagonal up left
+    //
+    if col > 0 && row > 0 {
+        if map[row-1][col-1] == seat_type {
+            adjacent_count += 1;
+        }
+    }
+
+    // Cover diagonal up right
+    //
+    if  row > 0 && col < map[row].len()-1 {
+        if map[row-1][col+1] == seat_type {
+            adjacent_count += 1;
+        }
+    }
+
+    // Cover diagonal down left
+    //
+    if col > 0 && row < map.len()-1 {
+        if map[row+1][col-1] == seat_type {
+            adjacent_count += 1;
+        }
+    }
+
+    // Cover diagonal down right
+    //
+    if  row < map.len()-1 && col < map[row].len()-1 {
+        if map[row+1][col+1] == seat_type {
+            adjacent_count += 1;
+        }
+    }
+
+    adjacent_count
 }
 
+fn dump_seat_map( seat_map : &Vec<Vec<char>> ) -> i32 {
+
+    let mut seat_count = 0;
+
+        for row in 0..seat_map.len() {
+
+            for col in 0..seat_map[row].len() {
+
+                if seat_map[row][col] == '#' {
+                    seat_count = seat_count + 1;
+                }
+                print!("{}", seat_map[row][col] );
+            }
+
+        println!();
+    }
+
+    seat_count
+}
 
 
 // The output is wrapped in a Result to allow matching on errors
