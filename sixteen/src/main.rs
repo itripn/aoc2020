9589cde9a33066790afdc8edc1e5734af915f4fc
Ron Forrester <rjf@m1.home>
2020-12-18 22:00:27 -0800

    commenting pass

--- sixteen/src/main.rs
+++ sixteen/src/main.rs
@@ -9,16 +9,28 @@ fn main() {
     let _my_ticket : Vec<i32>;
     let mut their_tickets : Vec<Vec<i32>> = Vec::new();
 
-    let all_tickets = &tickets.clone().into_iter()
+    // Read all the range rules from the input
+    //
+    let all_rules = &tickets.clone()
+        .into_iter()
+        .filter(|s| !s.starts_with(|p:char| p.is_digit(10) ) && !s.ends_with(":") && s.len()!=0 )
+        .collect::<Vec<&str>>();
+
+    // Explode all the ranges for testing against later...
+    //
+    explode_ranges( &all_rules, &mut exploded_ranges );
+
+
+    // Read all the other passenger tickets
+    //
+    let all_tickets = &tickets.clone()
+        .into_iter()
         .filter(|s| s.starts_with(|p:char| p.is_digit(10) )  )
         .collect::<Vec<&str>>();
 
-    let all_rules = &tickets.into_iter()
-        .filter(|s| !s.starts_with(|p:char| p.is_digit(10) ) && !s.ends_with(":") && s.len()!=0 )
-        .collect::<Vec<&str>>();
-    
-    explode_ranges( &all_rules, &mut exploded_ranges );
-
+    // Grab our ticket which is the first ticket
+    // in the list
+    //
     _my_ticket = all_tickets
         .first()
         .unwrap()
@@ -26,22 +38,35 @@ fn main() {
         .map(|n| n.parse::<i32>().unwrap() )
         .collect();
 
-    for t in &all_tickets[1..] {
-        let nt = t.split(',').map(|n| n.parse().unwrap()).collect();
-        their_tickets.push( nt );
-    }
+    // Loop through the rest of the tickets and get them ready
+    // for testing
+    //
+    all_tickets
+        .iter()
+        .skip(1)
+        .for_each(|t| {
+            let nt = t.split(',').map(|n| n.parse().unwrap()).collect();
+            their_tickets.push( nt );
+        });
+        
 
+    // Now for all the passenger tickets except ours, 
+    // validate whether the numbers are in the valid
+    // ranges for each field
+    //
     let total : i32 = their_tickets
         .into_iter()
         .flatten()
-        .collect::<Vec<i32>>()
-        .into_iter()
-        .filter(|n| !exploded_ranges.contains(n))
+        .filter(|n| !exploded_ranges.contains( n ) )
         .sum();
 
     println!("\nTotal scanning error rate: {}\n", total);
 }
 
+// Take in the range rules for each field and
+// combine them so that we can validate values against 
+// them.
+//
 fn explode_ranges( all_rules : &Vec<&str>, exploded_ranges : &mut Vec<i32> ) {
 
     let ranges_re = Regex::new(r"^(.*): ([0-9]*)-([0-9]*) or ([0-9]*)-([0-9]*)").unwrap();
@@ -49,6 +74,7 @@ fn explode_ranges( all_rules : &Vec<&str>, exploded_ranges : &mut Vec<i32> ) {
     for rule in all_rules {
 
         let caps = ranges_re.captures(rule);
+
         match caps {
 
             Some(_c) => {
